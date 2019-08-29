bce1cb9a66c1e145276cf8801e1a671c52d43152
Schmandre <47451170+Schmandre@users.noreply.github.com>
2019-08-29 13:42:04 +0200

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,23 +3,35 @@ import socket
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
+#INIT
 senderEmail = "sender@domain.com"
 empfangsEmail = "empfaenger@domain.com"
+smtpServer = "CHANGEME"
+smtpServerPort = 25
+nameKunde = "CHANGEME"
+
+#Get local IP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip=s.getsockname()[0]
+s.close()
+
 msg = MIMEMultipart()
 msg['From'] = senderEmail
 msg['To'] = empfangsEmail
 msg['X-Priority'] = '2'
-msg['Subject'] = "Deine TV ID hat sich geaendert"
+msg['Subject'] = "Eine TV ID bei "+nameKunde+" hat sich geaendert"
 hostname = socket.gethostname()
 tvIDOld = open("shortIDOld.txt").readlines()
 tvIDNew = open("shortID.txt").readlines()
+mailUsername=open("login.txt").readlines()
+mailPassword=open("login.txt").readlines()
 
-emailText = "Die Teamviewer ID vom RAPI "+hostname+" hat sich von "+str(tvIDOld)+" auf "+str(tvIDNew)+" geaendert"
+emailText = "Kunde: "+nameKunde+" <br> Hostname: "+hostname+" <br> IP: "+str(ip)+" <br> TeamViewer ID alt: "+str(tvIDOld)+" <br> TeamViewer ID neu: "+str(tvIDNew)+""
 msg.attach(MIMEText(emailText, 'html'))
-
-server = smtplib.SMTP('smtp.gmail.com', 587) #SMTP Server
+server = smtplib.SMTP(smtpServer, smtpServerPort) #SMTP Server
 server.starttls()
-server.login(senderEmail, "Passwort") #Passwort der Sendeadresse
+server.login(mailUsername[0], mailPassword[1]) #Passwort der Sendeadresse
 text = msg.as_string()
 server.sendmail(senderEmail, empfangsEmail, text)
 server.quit()
