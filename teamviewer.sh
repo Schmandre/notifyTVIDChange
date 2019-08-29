d553d886c365fbe3ccf8857dbab862c1db5f4334
Schmandre <47451170+Schmandre@users.noreply.github.com>
2019-08-29 14:11:37 +0200

    Fixed logic error
    Fixed an error which shows an error message on first run

--- teamviewer.sh
+++ teamviewer.sh
@@ -3,16 +3,19 @@ FILE=/root/shortID.txt
 if [ -f "$FILE" ]; then
     echo "$FILE exist"
 	mv /root/shortID.txt /root/shortIDOld.txt
+	python idExport.py > longID.txt
+	sleep 10
+	python read.py
+	sleep 10
+	DIFF=$(diff shortID.txt shortIDOld.txt) 
+	if [ "$DIFF" != "" ] 
+	then
+		cho "Die TeamViewer ID's haben sich geaendert"
+		python mail.py
+	fi
 else 
     echo "$FILE does not exist"
-fi
-python idExport.py > longID.txt
-sleep 10
-python read.py
-sleep 10
-DIFF=$(diff shortID.txt shortIDOld.txt) 
-if [ "$DIFF" != "" ] 
-then
-    echo "Die TeamViewer ID's haben sich geaendert"
-	python mail.py
+	python idExport.py > longID.txt
+	sleep 10
+	python read.py
 fi
