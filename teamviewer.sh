a11de548d64ee992c51aa135f95d8f8e3853e374
Schmandre <47451170+Schmandre@users.noreply.github.com>
2019-08-30 12:07:07 +0200

    indent code

--- teamviewer.sh
+++ teamviewer.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-FILE=/root/shortID.txt
-if [ -f "$FILE" ]; then
-    echo "$FILE exist"
-	mv /root/shortID.txt /root/shortIDOld.txt
-	python idExport.py > longID.txt
-	sleep 10
-	python read.py
-	sleep 10
-	DIFF=$(diff shortID.txt shortIDOld.txt) 
-	if [ "$DIFF" != "" ] 
-	then
-		echo "Die TeamViewer ID's haben sich geaendert"
-		python mail.py
-	fi
-else 
-    echo "$FILE does not exist"
-	python idExport.py > longID.txt
-	sleep 10
-	python read.py
-fi
+FILE=/root
+if [ -f "$FILE/shortID.txt" ]; then
+    echo "$FILE/shortID.txt exist"
+	  mv $FILE/shortID.txt /root/shortIDOld.txt
+    python idExport.py > longID.txt
+    sleep 10
+    python read.py
+    sleep 10
+    DIFF=$(diff shortID.txt shortIDOld.txt)
+    if [ "$DIFF" != "" ]
+    then
+      echo "Die TeamViewer ID's haben sich geaendert"
+      python mail.py
+    fi
+    else
+      echo "$FILE does not exist"
+      python idExport.py > longID.txt
+      sleep 10
+      python read.py
+    fi
 rm longID.txt
